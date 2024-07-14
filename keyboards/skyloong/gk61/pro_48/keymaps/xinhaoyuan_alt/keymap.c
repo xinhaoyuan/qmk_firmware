63f03f58be21d0d141a3378bf6866219b2ed42de
Xinhao Yuan <xinhaoyuan@gmail.com>
2024-07-14 12:24:28 -0400

    Fixes

--- keyboards/skyloong/gk61/pro_48/keymaps/xinhaoyuan_alt/keymap.c
+++ keyboards/skyloong/gk61/pro_48/keymaps/xinhaoyuan_alt/keymap.c
@@ -36,21 +36,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,       KC_I,     KC_O,        KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,
         KC_LCTL,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,     KC_J,       KC_K,     KC_L,     KC_SCLN,     KC_QUOT,              KC_ENT,
         KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,        KC_N,     KC_M,    KC_COMM,   KC_DOT,     KC_SLSH,               KC_RSFT,
-         MY_FN1,  KC_LGUI,  KC_LALT,             KC_SPC,   KC_SPC,     KC_MUTE,               MY_FN2,  KC_RALT,     KC_APP,     KC_RCTL,              MO(SYS)
+         MY_FN2,  KC_LGUI,  KC_LALT,             KC_SPC,   KC_SPC,     KC_MUTE,               MY_FN2,  KC_RALT,      KC_APP,     KC_RCTL,              MO(SYS)
     ),
 
     [FN1] = LAYOUT_all(
         _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,       KC_F6,    KC_F7,      KC_F8,    KC_F9,      KC_F10,      KC_F11,   KC_F12,    KC_DEL,
-          KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,  KC_WH_L,    KC_MS_U,  KC_WH_R,     KC_WH_U,       KC_NO,    KC_NO,     KC_NO,
-        _______,    KC_NO,  KC_BTN3,  KC_BTN2,  KC_BTN1,    KC_NO,       KC_NO,  KC_MS_L,    KC_MS_D,  KC_MS_R,     KC_WH_D,      KC_GRV,             _______,
-        _______,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,  KC_BTN1,    KC_BTN2,  KC_BTN3,       KC_NO,               _______,
+          KC_NO,  KC_WH_U,  KC_WH_L,  KC_MS_U,  KC_WH_R,    KC_NO,       KC_NO,  KC_WH_L,    KC_MS_U,  KC_WH_R,     KC_WH_U,       KC_NO,    KC_NO,     KC_NO,
+        _______,  KC_WH_D,  KC_MS_L,  KC_MS_D,  KC_MS_R,    KC_NO,       KC_NO,  KC_MS_L,    KC_MS_D,  KC_MS_R,     KC_WH_D,      KC_GRV,             _______,
+        _______,    KC_NO,  KC_BTN3,  KC_BTN2,  KC_BTN1,    KC_NO,       KC_NO,  KC_BTN1,    KC_BTN2,  KC_BTN3,       KC_NO,               _______,
         _______,  _______,  _______,            _______,  _______,     _______,              _______,  _______,     _______,     _______,             _______
     ),
 
     [FN2] = LAYOUT_all(
         _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,       KC_F6,    KC_F7,      KC_F8,    KC_F9,      KC_F10,      KC_F11,   KC_F12,    KC_DEL,
-          KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,  KC_HOME,      KC_UP,   KC_END,     KC_PGUP,       KC_NO,    KC_NO,     KC_NO,
-        _______,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,  KC_LEFT,    KC_DOWN, KC_RIGHT,     KC_PGDN,      KC_GRV,             _______,
+          KC_NO,  KC_PGUP,  KC_HOME,    KC_UP,   KC_END,    KC_NO,       KC_NO,  KC_HOME,      KC_UP,   KC_END,     KC_PGUP,       KC_NO,    KC_NO,     KC_NO,
+        _______,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,    KC_NO,       KC_NO,  KC_LEFT,    KC_DOWN, KC_RIGHT,     KC_PGDN,      KC_GRV,             _______,
         _______,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,    KC_NO,      KC_NO,    KC_NO,       KC_NO,               _______,
         MO(SYS),  _______,  _______,            _______,  _______,     _______,              _______,  _______,     _______,     _______,             _______
     ),
@@ -183,9 +183,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
         }
         return false;
-    default:
-        return true; // Process all other keycodes normally
     }
+    if (record->event.pressed && space_hold) {
+        register_code(KC_SPACE);
+        space_hold = 0;
+    }
+    return true;
 #undef IS_TAPPING
 }
 
