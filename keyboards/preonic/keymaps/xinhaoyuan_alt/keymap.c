7be2d65b66b1f8dc2eaeb508a2134ca975d1699c
Xinhao Yuan <xinhaoyuan@gmail.com>
2024-08-07 22:40:28 -0400

    A few tweaks and planck config.

--- keyboards/preonic/keymaps/xinhaoyuan_alt/keymap.c
+++ keyboards/preonic/keymaps/xinhaoyuan_alt/keymap.c
@@ -33,26 +33,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [L_BASE] = LAYOUT_preonic_grid(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_ESC,MO(L_SYS), KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RGUI, KC_RCTL, KC_BSPC
+        RAISE,   KC_ESC,  KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RGUI, KC_RCTL, LOWER
         ),
 
     [L_LOWER] = LAYOUT_preonic_grid(
         KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         KC_NO,   KC_WH_U, KC_WH_L, KC_MS_U, KC_WH_R, KC_NO,   KC_NO,   KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U, KC_F12,
         _______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_RBRC, KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_NO,
-        _______, KC_GRV,  KC_BTN3, KC_BTN2, KC_BTN1, KC_LBRC, KC_MINS, KC_BTN1, KC_BTN2, KC_BTN3, KC_QUOT, _______,
-        KC_NO,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_NO
+        _______, KC_GRV,  KC_BTN3, KC_BTN2, KC_BTN1, KC_LBRC, KC_MINS, KC_BTN1, KC_BTN2, KC_BTN3, KC_BSLS, _______,
+        _______, KC_NO,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
         ),
 
     [L_RAISE] = LAYOUT_preonic_grid(
         KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         KC_NO,   KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_F12,
-        _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RBRC, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO,
-        _______, KC_GRV,  KC_NO,   KC_NO,   KC_NO,   KC_LBRC, KC_MINS, KC_LBRC, KC_RBRC, KC_NO,   KC_QUOT, _______,
-        KC_NO,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_NO
+        _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO,
+        _______, KC_GRV,  KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_NO,   KC_MINS, KC_RBRC, KC_NO,   KC_BSLS, _______,
+        _______, KC_NO,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
         ),
 
     [L_PARA] = LAYOUT_preonic_grid(
@@ -60,15 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_VOLU, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_VOLD, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MUTE, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______,
-        KC_NO,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_NO 
-        ),
-
-    [L_SYS] = LAYOUT_preonic_grid(
-        QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______,
-        KC_NO,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_NO
+        _______, QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
         ),
 
 };
