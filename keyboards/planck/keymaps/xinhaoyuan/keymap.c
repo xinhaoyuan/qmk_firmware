1c6b5257edcee14aed5a2d02a4a20369e2c7d044
Xinhao Yuan <xinhaoyuan@gmail.com>
2024-08-20 21:56:42 -0400

    Redo the oneshot and mod alternating logic.

--- keyboards/planck/keymaps/xinhaoyuan/keymap.c
+++ keyboards/planck/keymaps/xinhaoyuan/keymap.c
@@ -33,36 +33,27 @@ enum keycodes {
     LPTOGG,
 };
 
-#define OS_LSFT OSM(MOD_LSFT)
-#define OS_LALT OSM(MOD_LALT)
-#define OS_LCTL OSM(MOD_LCTL)
-#define OS_LGUI OSM(MOD_LGUI)
-#define OS_RSFT OSM(MOD_RSFT)
-#define OS_RALT OSM(MOD_RALT)
-#define OS_RCTL OSM(MOD_RCTL)
-#define OS_RGUI OSM(MOD_RGUI)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [L_BASE] = LAYOUT_planck_grid(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RGUI,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        RAISE,   KC_ESC,  KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_RALT, KC_RCTL, KC_ESC,  LOWER
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
+        RAISE,   KC_ESC,  KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_RSFT, KC_RCTL, KC_ESC,  LOWER
     ),
 
     [L_LOWER] = LAYOUT_planck_grid(
         KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-        OS_LCTL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    OS_RGUI,
-        OS_LSFT, KC_GRV,  KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  KC_NO,   KC_QUOT, KC_MINS, KC_EQL,  KC_BSLS, OS_RSFT,
-        _______, KC_NO,   OS_LGUI, OS_LALT, _______, KC_SPC,  KC_ENT,  _______, OS_RALT, OS_RCTL, KC_NO,   _______
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        _______, KC_GRV,  KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  KC_NO,   KC_QUOT, KC_MINS, KC_EQL,  KC_BSLS, _______,
+        _______, KC_NO,   _______, _______, _______, KC_SPC,  KC_ENT,  _______, _______, _______, KC_NO,   _______
     ),
 
     [L_RAISE] = LAYOUT_planck_grid(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_DEL,
-        OS_LCTL, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, OS_RGUI,
-        OS_LSFT, KC_TILD, KC_LCBR, KC_RCBR, KC_F11,  KC_F12,  KC_NO,   KC_DQT,  KC_UNDS, KC_PLUS, KC_PIPE, OS_RSFT,
-        _______, KC_NO,   OS_LGUI, OS_LALT, _______, KC_SPC,  KC_ENT,  _______, OS_RALT, OS_RCTL, KC_NO,   _______
+        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,
+        _______, KC_TILD, KC_LCBR, KC_RCBR, KC_F11,  KC_F12,  KC_NO,   KC_DQT,  KC_UNDS, KC_PLUS, KC_PIPE, _______,
+        _______, KC_NO,   _______, _______, _______, KC_SPC,  KC_ENT,  _______, _______, _______, KC_NO,   _______
     ),
 
     [L_MOUSE] = LAYOUT_planck_grid(
@@ -101,6 +92,9 @@ static uint16_t pressed_keycode = KC_NO;
 static uint16_t pressed_time = 0;
 static uint16_t last_pressed_keycode = KC_NO;
 static uint16_t last_pressed_time = 0;
+static uint16_t last_tapped_keycode = KC_NO;
+static uint8_t my_mod_mask = 0;
+static uint8_t my_alt_mod_mask = 0;
 
 static void clean_up_oneshot(void) {
     uint8_t mods;
@@ -111,6 +105,7 @@ static void clean_up_oneshot(void) {
 }
 
 #define ALWAYS_SET_LAYER 1
+#define IN_MOD() (layer_state_is(L_LOWER) || layer_state_is(L_RAISE))
 
 static void layer_lock(int layer) {
     ++layer_lock_count;
@@ -158,6 +153,64 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     } else {
         pressed_keycode = KC_NO;
     }
+    if (!record->event.pressed) {
+        if (last_pressed_keycode == keycode) {
+            last_tapped_keycode = keycode;
+        } else {
+            last_tapped_keycode = KC_NO;
+        }
+    }
+
+#define HANDLE_MOD(ALT_KEY)                                             \
+    do {                                                                \
+        if (record->event.pressed) {                                    \
+            if (IN_MOD()) {                                             \
+                if ((get_oneshot_mods() & MOD_BIT(keycode)) == 0) {     \
+                    add_oneshot_mods(MOD_BIT(keycode));                 \
+                    add_mods(MOD_BIT(keycode));                         \
+                    my_mod_mask |= MOD_BIT(keycode);                    \
+                } else {                                                \
+                    del_oneshot_mods(MOD_BIT(keycode));                 \
+                    del_mods(MOD_BIT(keycode));                         \
+                    my_mod_mask ^= MOD_BIT(keycode);                    \
+                    add_oneshot_mods(MOD_BIT(ALT_KEY));                 \
+                    add_mods(MOD_BIT(ALT_KEY));                         \
+                    my_alt_mod_mask |= MOD_BIT(keycode);                \
+                }                                                       \
+            } else {                                                    \
+                if (last_tapped_keycode == keycode &&                   \
+                    timer_elapsed(last_pressed_time) < 300)  {          \
+                    my_alt_mod_mask |= MOD_BIT(keycode);                \
+                    add_mods(MOD_BIT(ALT_KEY));                         \
+                } else {                                                \
+                    add_mods(MOD_BIT(keycode));                         \
+                    my_mod_mask |= MOD_BIT(keycode);                    \
+                    break;                                              \
+                }                                                       \
+            }                                                           \
+        } else {                                                        \
+            if ((my_alt_mod_mask & MOD_BIT(keycode))) {                 \
+                my_alt_mod_mask ^= MOD_BIT(keycode);                    \
+                del_mods(MOD_BIT(ALT_KEY));                             \
+            }                                                           \
+            if ((my_mod_mask & MOD_BIT(keycode))) {                     \
+                my_mod_mask ^= MOD_BIT(keycode);                        \
+                del_mods(MOD_BIT(keycode));                             \
+                break;                                                  \
+            }                                                           \
+        }                                                               \
+        return false;                                                   \
+    } while (0)
+
+#define HANDLE_MOUSE_LAYER()                                            \
+    do {                                                                \
+        if (!record->event.pressed) {                                   \
+            if (llast_pressed_keycode == keycode && (last_pressed_keycode == LOWER || last_pressed_keycode == RAISE)) { \
+                layer_unlock(0);                                        \
+                layer_lock(L_MOUSE);                                    \
+            }                                                           \
+        }                                                               \
+    } while (0)
 
 #define IS_TAPPING() (last_pressed_keycode == keycode && timer_elapsed(last_pressed_time) < 300)
     switch (keycode) {
@@ -175,14 +228,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             layer_unlock(L_RAISE);
         }
         return false;
+    case KC_LCTL:
+        HANDLE_MOD(KC_LGUI);
+        break;
+    case KC_RCTL:
+        HANDLE_MOD(KC_RGUI);
+        break;
+    case KC_LALT:
+        HANDLE_MOD(KC_LSFT);
+        break;
+    case KC_RALT:
+        HANDLE_MOUSE_LAYER();
+        HANDLE_MOD(KC_RSFT);
+        break;
+    case KC_LGUI:
+        HANDLE_MOD(KC_LCTL);
+        break;
+    case KC_RGUI:
+        HANDLE_MOD(KC_RCTL);
+        break;
     case KC_LSFT:
+        HANDLE_MOUSE_LAYER();
+        HANDLE_MOD(KC_LALT);
+        break;
     case KC_RSFT:
-        if (!record->event.pressed) {
-            if (llast_pressed_keycode == keycode && (last_pressed_keycode == LOWER || last_pressed_keycode == RAISE)) {
-                layer_unlock(0);
-                layer_lock(L_MOUSE);
-            }
-        }
+        HANDLE_MOD(KC_RALT);
         break;
     case LPSWCH:
         if (record->event.pressed) {
@@ -208,4 +278,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 #undef IS_TAPPING
+#undef HANDLE_MOD
+#undef HANDLE_MOUSE_LAYER
 }
