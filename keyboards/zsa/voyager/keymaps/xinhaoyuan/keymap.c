95d4c681952d812a8844dad1bbdee4f7afe49874
Xinhao Yuan <xinhaoyuan@gmail.com>
2024-09-13 07:47:01 -0400

    A few updates; new chiri layout.

--- keyboards/zsa/voyager/keymaps/xinhaoyuan/keymap.c
+++ keyboards/zsa/voyager/keymaps/xinhaoyuan/keymap.c
@@ -265,7 +265,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_LOWER] = _LAYOUT(
         KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        _______, KC_GRV,  KC_LBRC, KC_RBRC, CW_TOGG, KC_NO,   KC_NO,   KC_QUOT, KC_MINS, KC_EQL,  KC_BSLS, _______,
+        _______, KC_GRV,  KC_LBRC, KC_RBRC, CW_TOGG, MY_LGUI, KC_NO,   KC_QUOT, KC_MINS, KC_EQL,  KC_BSLS, _______,
         _______, KC_NO,   _______, _______, _______, KC_SPC,  KC_ENT,  _______, _______, _______, KC_NO,   _______,
                                             KC_NO,   KC_NO,   KC_NO,   KC_NO
     ),
@@ -273,7 +273,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_RAISE] = _LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_DEL,
         _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,
-        _______, KC_TILD, KC_LCBR, KC_RCBR, KC_F11,  KC_F12,  KC_NO,   KC_DQT,  KC_UNDS, KC_PLUS, KC_PIPE, _______,
+        _______, KC_TILD, KC_LCBR, KC_RCBR, KC_F11,  KC_F12,  MY_RCTL, KC_DQT,  KC_UNDS, KC_PLUS, KC_PIPE, _______,
         _______, KC_NO,   _______, _______, _______, KC_SPC,  KC_ENT,  _______, _______, _______, KC_NO,   _______,
                                             KC_NO,   KC_NO,   KC_NO,  KC_NO
     ),
@@ -303,9 +303,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [L_PARA] = _LAYOUT(
-        KC_NO,   RGB_HUI, RGB_SAI, RGB_VAI, KC_NO,   KC_VOLU, LPTOGG,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        _______, RGB_HUD, RGB_SAD, RGB_VAD, KC_NO,   KC_VOLD, LPSWCH,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______,
-        _______, RGB_TOG, RGB_M_P, KC_NO,   KC_CAPS, KC_MUTE, KC_NO,   AMTOGG,  KC_NO,   KC_NO,   KC_NO,   _______,
+        QK_BOOT, UG_HUEU, UG_SATU, UG_VALU, KC_NO,   KC_VOLU, LPTOGG,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        _______, UG_HUED, UG_SATD, UG_VALD, KC_NO,   KC_VOLD, LPSWCH,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______,
+        _______, UG_TOGG, RGB_M_P, UG_NEXT, KC_CAPS, KC_MUTE, KC_NO,   AMTOGG,  KC_NO,   KC_NO,   KC_NO,   _______,
         _______, QK_BOOT, _______, _______, _______, KC_NO,   KC_NO,   _______, _______, _______, KC_NO,   _______,
                                             KC_NO,   KC_NO,   KC_NO,   KC_NO
     ),
