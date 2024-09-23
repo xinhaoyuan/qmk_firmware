41f0485796a73b4642eaf07dee330c7a33e7f96c
Xinhao Yuan <xinhaoyuan@gmail.com>
2024-09-23 18:29:44 -0400

    Improve split flash script.

--- util/flash_split.sh
+++ util/flash_split.sh
@@ -8,9 +8,12 @@ TARGET="$1"
 
 # "${DIR}/docker_build.sh" "${TARGET}:build"
 
-echo "Flashing the left side in 15s" >&2
-sleep 15
-pmount sda1
+echo "Flashing the left side in 5s" >&2
+sleep 5
+while ! pmount sda1; do
+    echo "Retry to mount sda1 for the left side in 1s" >&2
+    sleep 1
+done
 if ! "${DIR}/docker_build.sh" "${TARGET}:uf2-split-left"; then
     echo "Failed to flash the left side. Right side untouched." >&2
     pumount sda1
@@ -18,9 +21,12 @@ if ! "${DIR}/docker_build.sh" "${TARGET}:uf2-split-left"; then
 fi
 pumount sda1
 
-echo "Flashing the right side in 15s" >&2
-sleep 15
-pmount sda1
+echo "Flashing the right side in 5s" >&2
+sleep 5
+while ! pmount sda1; do
+    echo "Retry to mount sda1 for the right side in 1s" >&2
+    sleep 1
+done
 if ! "${DIR}/docker_build.sh" "${TARGET}:uf2-split-right"; then
     echo "Failed to flash the right side." >&2
     pumount sda1
